f6d11b1a111e9e690ca3938fa758cb9059737ead
forxtu <forxtu@gmail.com>
2019-11-05 11:21:25 +0100

    Updates

---
+++ precache-manifest.a94277a63a8d54935873a968f275c091.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9177892d02ce5ba4793a9c9f1cd5cf",
+    "url": "/bb-calendar/index.html"
+  },
+  {
+    "revision": "ae22107b649e745ef81b",
+    "url": "/bb-calendar/static/css/main.20946b74.chunk.css"
+  },
+  {
+    "revision": "3f02f78728d55c942e1b",
+    "url": "/bb-calendar/static/js/2.f79153fa.chunk.js"
+  },
+  {
+    "revision": "ae22107b649e745ef81b",
+    "url": "/bb-calendar/static/js/main.5fdb2f2f.chunk.js"
+  },
+  {
+    "revision": "ab1ff431cf81fe6e0f61",
+    "url": "/bb-calendar/static/js/runtime-main.8b05e7ec.js"
+  },
+  {
+    "revision": "4152e7959a1906c452bd583971a950f5",
+    "url": "/bb-calendar/static/media/CooperHewitt-Bold.4152e795.ttf"
+  },
+  {
+    "revision": "c72efb3acbc95c213a88817793592f09",
+    "url": "/bb-calendar/static/media/CooperHewitt-Bold.c72efb3a.woff"
+  },
+  {
+    "revision": "fb5f34fc1978e33204ed294b308d8b5f",
+    "url": "/bb-calendar/static/media/CooperHewitt-Bold.fb5f34fc.woff2"
+  },
+  {
+    "revision": "fe74b516b9279dfb9a96adba002e5d17",
+    "url": "/bb-calendar/static/media/CooperHewitt-Bold.fe74b516.eot"
+  },
+  {
+    "revision": "50af1f5bb14cebeb138e7e046ae4368b",
+    "url": "/bb-calendar/static/media/GT-Pressura-Regular.50af1f5b.ttf"
+  },
+  {
+    "revision": "650a07439ec72945a99feb0a5d18b475",
+    "url": "/bb-calendar/static/media/GT-Pressura-Regular.650a0743.woff"
+  },
+  {
+    "revision": "ce2b4158ad98d3a87370408b2a84fec0",
+    "url": "/bb-calendar/static/media/GT-Pressura-Regular.ce2b4158.eot"
+  },
+  {
+    "revision": "fa7baf1f63e4fc08db4b4932dbfb953f",
+    "url": "/bb-calendar/static/media/GT-Pressura-Regular.fa7baf1f.woff2"
+  },
+  {
+    "revision": "4a5d846562e55f1c1402795b05072ce2",
+    "url": "/bb-calendar/static/media/GTPressuraMono.4a5d8465.eot"
+  },
+  {
+    "revision": "8fb70c49bc8a4357604ac557ea701124",
+    "url": "/bb-calendar/static/media/GTPressuraMono.8fb70c49.woff2"
+  },
+  {
+    "revision": "a359fdae907a10546cbd6d25a747785c",
+    "url": "/bb-calendar/static/media/GTPressuraMono.a359fdae.woff"
+  },
+  {
+    "revision": "b43420dda1a968d3dc719d7c0e2d8870",
+    "url": "/bb-calendar/static/media/GTPressuraMono.b43420dd.ttf"
+  },
+  {
+    "revision": "0d43b34c34fd589c5546d2cfb9a921c6",
+    "url": "/bb-calendar/static/media/calendar.0d43b34c.svg"
+  },
+  {
+    "revision": "54ef1ff3570aae704255487400409917",
+    "url": "/bb-calendar/static/media/van.54ef1ff3.svg"
+  }
+]);
